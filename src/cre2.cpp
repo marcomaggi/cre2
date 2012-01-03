5072ec3f7f76afca7681d3e3c33c53d941b4db6f
Marco Maggi <marco.maggi-ipsu@poste.it>
2012-01-03 07:38:51 +0100

    normalised return value name for matching functions

--- src/cre2.cpp
+++ src/cre2.cpp
@@ -139,9 +139,10 @@ cre2_match (const cre2 *re , const char *text,
 	    int textlen, int startpos, int endpos, cre2_anchor_t anchor,
 	    cre2_string_t *match, int nmatch)
 {
-  re2::StringPiece text_re2(text, textlen);
-  re2::StringPiece match_re2[nmatch];
-  RE2::Anchor anchor_re2 = RE2::UNANCHORED;
+  re2::StringPiece	text_re2(text, textlen);
+  re2::StringPiece	match_re2[nmatch];
+  RE2::Anchor		anchor_re2 = RE2::UNANCHORED;
+  bool			retval; // 0 for no match, 1 for successful matching
   switch (anchor) {
   case CRE2_ANCHOR_START:
     anchor_re2 = RE2::ANCHOR_START;
@@ -150,14 +151,14 @@ cre2_match (const cre2 *re , const char *text,
     anchor_re2 = RE2::ANCHOR_BOTH;
     break;
   }
-  bool ret = TO_CONST_RE2(re)->Match(text_re2, startpos, endpos, anchor_re2, match_re2, nmatch);
+  retval = TO_CONST_RE2(re)->Match(text_re2, startpos, endpos, anchor_re2, match_re2, nmatch);
   if (ret) {
     for (int i=0; i<nmatch; i++) {
       match[i].data   = match_re2[i].data();
       match[i].length = match_re2[i].length();
     }
   }
-  return int(ret);
+  return (retval)? 1 : 0;
 }
 int
 cre2_easy_match (const char * pattern, int pattern_len,
@@ -166,20 +167,21 @@ cre2_easy_match (const char * pattern, int pattern_len,
 {
   cre2 *		rex;
   cre2_options *	opt;
-  int			matched;
+  int			retval; // 0  for  no  match, 1  for  successful
+				// matching, 2 for wrong regexp
   opt	= cre2_opt_new();
   cre2_opt_log_errors(opt, 0);
   rex	= cre2_new(pattern, pattern_len, opt);
   {
     if (cre2_ok(rex)) {
-      matched = cre2_match(rex, text, text_len, 0, text_len, CRE2_UNANCHORED, match, nmatch);
+      retval = cre2_match(rex, text, text_len, 0, text_len, CRE2_UNANCHORED, match, nmatch);
     } else {
-      matched = 2;
+      retval = 2;
     }
   }
   cre2_delete(rex);
   cre2_opt_delete(opt);
-  return matched;
+  return retval;
 }
 
 /* end of file */
