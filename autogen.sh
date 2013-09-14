bac524dfda5ee865dab9fc757bb113282665569b
Marco Maggi <marco.maggi-ipsu@poste.it>
2013-09-14 13:08:40 +0200

    review of autogen script

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,9 @@
 #
 # Run this in the top source directory to rebuild the infrastructure.
 
-export PATH=/bin:/usr/local/bin:/usr/bin:$PATH
-
 set -xe
-test -d autotools || mkdir autotools
+test -d autotools		|| mkdir autotools
+test -f autotools/libtool.m4	|| libtoolize
 autoreconf --warnings=all --install --verbose "$@"
 
 ### end of file
