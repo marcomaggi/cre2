db6dbc641b1abff05c3e736963798cafb3fb1c3d
Marco Maggi <marco.maggi-ipsu@poste.it>
2014-03-22 11:26:40 +0100

    moved to single Makefile.am

--- configure.sh
+++ configure.sh
@@ -5,6 +5,10 @@
 set -xe
 
 prefix=/usr/local
+if test -d /lib64
+then libdir=${prefix}/lib64
+else libdir=${prefix}/lib
+fi
 
 ../configure \
     --config-cache                              \
@@ -12,7 +16,9 @@ prefix=/usr/local
     --enable-maintainer-mode                    \
     --disable-static --enable-shared            \
     --prefix="${prefix}"                        \
-    CFLAGS='-O3' LDFLAGS='-L/usr/local/lib'	\
+    --libdir="${libdir}"			\
+    CFLAGS='-O3'				\
+    LDFLAGS="-L${libdir}"			\
     "$@"
 
 ### end of file
