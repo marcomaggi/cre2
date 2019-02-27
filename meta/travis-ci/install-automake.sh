ca659eb50c8151e3c5b49b8e178024d42428aa39
Marco Maggi <marco.maggi-ipsu@poste.it>
2019-02-27 15:08:02 +0100

    development of travis build scripts

--- meta/travis-ci/install-automake.sh
+++ meta/travis-ci/install-automake.sh
@@ -1,18 +1,57 @@
 #!/bin/bash
 #
-# Installation  script  to  run  from  the  Travis  config  file  before
+# Installation  script to  run from  the  Travis CI  config file  before
 # attempting a build.
 #
-# Install GNU Automake under the  directory "/usr/local".  We assume the
+# Install GNU Automake under the directory "/usr/local".  We assume the
 # script is run from the top directory of the build tree.
 
-PROGNAME="${0##*/}"
-STEM=automake-1.16.1
-ARCHIVE="${STEM}.tar.gz"
-SOURCE_URI="http://marcomaggi.github.io/binaries/${ARCHIVE}"
-LOCAL_ARCHIVE="/tmp/${ARCHIVE}"
-TOP_SRCDIR="/tmp/${STEM}"
+PROGNAME=${0##*/}
+PACKAGE_NAME=automake
+REQUIRED_PACKAGE_VERSION=1.16.1
+STEM=${PACKAGE_NAME}-${REQUIRED_PACKAGE_VERSION}
+VERSION_EXECUTABLE=$PACKAGE_NAME
+ARCHIVE=${STEM}.tar.gz
+SOURCE_URI=http://marcomaggi.github.io/binaries/${ARCHIVE}
+LOCAL_ARCHIVE=/tmp/${ARCHIVE}
+TOP_SRCDIR=/tmp/${STEM}
 prefix=/usr/local
+
+function main () {
+    # We install the new package only if it is not already installed.
+    if type -p "$VERSION_EXECUTABLE"
+    then
+	if "$VERSION_EXECUTABLE" --version | fgrep "$REQUIRED_PACKAGE_VERSION"
+	then exit 0
+	fi
+    fi
+
+    if ! wget --no-check-certificate "$SOURCE_URI" -O "$LOCAL_ARCHIVE"
+    then script_error 'error downloading %s' "$ARCHIVE"
+    fi
+
+    cd /tmp
+
+    if ! tar --extract --gzip --file="$LOCAL_ARCHIVE"
+    then script_error 'error unpacking %s' "$LOCAL_ARCHIVE"
+    fi
+
+    cd "$TOP_SRCDIR"
+
+    if ! ./configure --prefix="$prefix" --with-internal-glib
+    then script_error 'error configuring %s' "$STEM"
+    fi
+
+    if ! make -j2 all
+    then script_error 'error building all %s' "$STEM"
+    fi
+
+    if ! (umask 0; sudo make install)
+    then script_error 'error installing %s' "$STEM"
+    fi
+
+    exit 0
+}
 
 function script_error () {
     local TEMPLATE="${1:?missing template argument to '$FUNCNAME'}"
@@ -25,30 +64,6 @@ function script_error () {
     exit 1
 }
 
-if ! wget "$SOURCE_URI" -O "$LOCAL_ARCHIVE"
-then script_error 'error downloading %s' "$ARCHIVE"
-fi
-
-cd /tmp
-
-if ! tar --extract --gzip --file="$LOCAL_ARCHIVE"
-then script_error 'error unpacking %s' "$LOCAL_ARCHIVE"
-fi
-
-cd "$TOP_SRCDIR"
-
-if ! ./configure --prefix="$prefix"
-then script_error 'error configuring %s' "$STEM"
-fi
-
-if ! make -j2 all
-then script_error 'error building all %s' "$STEM"
-fi
-
-if ! (umask 0; sudo make install)
-then script_error 'error installing %s' "$STEM"
-fi
-
-exit 0
+main "$@"
 
 ### end of file
--- meta/travis-ci/install-texinfo.sh
+++ meta/travis-ci/install-texinfo.sh
@@ -1,5 +1,80 @@
 #!/bin/bash
 #
+# Installation  script to  run from  the  Travis CI  config file  before
+# attempting a build.
+#
+# Install GNU Texinfo  under the directory "/usr/local".   We assume the
+# script is run from the top directory of the build tree.
+
+PROGNAME=${0##*/}
+PACKAGE_NAME=texinfo
+REQUIRED_PACKAGE_VERSION=6.6
+STEM=${PACKAGE_NAME}-${REQUIRED_PACKAGE_VERSION}
+VERSION_EXECUTABLE=makeinfo
+ARCHIVE=${STEM}.tar.gz
+SOURCE_URI=http://marcomaggi.github.io/binaries/${ARCHIVE}
+LOCAL_ARCHIVE=/tmp/${ARCHIVE}
+TOP_SRCDIR=/tmp/${STEM}
+prefix=/usr/local
+
+function main () {
+    # We install the new package only if it is not already installed.
+    if type -p "$VERSION_EXECUTABLE"
+    then
+	if "$VERSION_EXECUTABLE" --version | fgrep "$REQUIRED_PACKAGE_VERSION"
+	then exit 0
+	fi
+    fi
+
+    if ! wget --no-check-certificate "$SOURCE_URI" -O "$LOCAL_ARCHIVE"
+    then script_error 'error downloading %s' "$ARCHIVE"
+    fi
+
+    cd /tmp
+
+    if ! tar --extract --gzip --file="$LOCAL_ARCHIVE"
+    then script_error 'error unpacking %s' "$LOCAL_ARCHIVE"
+    fi
+
+    cd "$TOP_SRCDIR"
+
+    if ! ./configure --prefix="$prefix" --with-internal-glib
+    then script_error 'error configuring %s' "$STEM"
+    fi
+
+    if ! make -j2 all
+    then script_error 'error building all %s' "$STEM"
+    fi
+
+    if ! (umask 0; sudo make install)
+    then script_error 'error installing %s' "$STEM"
+    fi
+
+    exit 0
+}
+
+function script_error () {
+    local TEMPLATE="${1:?missing template argument to '$FUNCNAME'}"
+    shift
+    {
+	printf '%s: ' "$PROGNAME"
+	printf "$TEMPLATE" "$@"
+	printf '\n'
+    } >&2
+    exit 1
+}
+
+main "$@"
+
+### end of file
+
+
+
+
+
+
+#!/bin/bash
+#
 # Installation  script  to  run  from  the  Travis  config  file  before
 # attempting a build.
 #
@@ -7,7 +82,7 @@
 # script is run from the top directory of the build tree.
 
 PROGNAME="${0##*/}"
-STEM=texinfo-6.6
+STEM=texinfo-
 ARCHIVE="${STEM}.tar.gz"
 SOURCE_URI="http://marcomaggi.github.io/binaries/${ARCHIVE}"
 LOCAL_ARCHIVE="/tmp/${ARCHIVE}"
